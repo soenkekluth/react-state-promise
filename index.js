69d81de65d53197ae16fc0fff01492d6a58e7b2f
Sönke Kluth <mail@soenkekluth.com>
2017-01-12 00:50:55 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const instanceMap = {};
+
+const setState = (instance, nextState) => {
+  if (instanceMap[instance]) {
+    instance.setState(nextState, instanceMap[instance].resolver);
+    return instanceMap[instance].promise;
+  }
+
+  let promiseResolver;
+  const promise = new Promise((resolve) => {
+    promiseResolver = resolve;
+  })
+  .then(() => {
+    const state = instance.state;
+    delete instanceMap[instance];
+    return state;
+  });
+
+  const resolver = () => {
+    promiseResolver(instance.state);
+  };
+
+  instanceMap[instance] = {
+    promise,
+    resolver,
+  };
+
+  instance.setState(nextState, instanceMap[instance].resolver);
+  return instanceMap[instance].promise;
+};
+
+export default setState;
